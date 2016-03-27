de159a2acc057465c21ccf9fc04cf03e3cc9dbac
Michael Kramer <kramer@localhost.localdomain>
2016-03-26 20:30:10 -0700

    Removed incorrect services

--- Virge/Stork/config/service.php
+++ Virge/Stork/config/service.php
@@ -1,10 +1,6 @@
 <?php
 
-use Virge\Stork\Service\PushMessagingService;
-use Virge\Stork\Service\WebsocketServer;
 use Virge\Stork\Service\ZMQMessagingService;
 use Virge\Virge;
 
-Virge::registerService(PushMessagingService::class, new PushMessagingService());
-Virge::registerService(WebsocketServer::class, new WebsocketServer());
-Virge::registerService(ZMQMessagingService::class, new ZMQMessagingService());
+Virge::registerService(ZMQMessagingService::SERVICE_ID, new ZMQMessagingService());
