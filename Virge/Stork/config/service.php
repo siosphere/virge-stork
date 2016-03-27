8a3db6ac552e96bb6fdbdf8a33b83220144fe4fa
Michael Kramer <kramer@localhost.localdomain>
2016-03-26 20:33:45 -0700

    updated service definition to use virge config

--- Virge/Stork/config/service.php
+++ Virge/Stork/config/service.php
@@ -1,6 +1,12 @@
 <?php
 
+use Virge\Core\Config;
 use Virge\Stork\Service\ZMQMessagingService;
 use Virge\Virge;
 
-Virge::registerService(ZMQMessagingService::SERVICE_ID, new ZMQMessagingService());
+$zmqServer = Config::get('stork', 'zmq_server');
+$zmqPort = Config::get('stork', 'zmq_port');
+
+$websocketServers = Config::get('stork', 'websocket_servers');
+
+Virge::registerService(ZMQMessagingService::SERVICE_ID, new ZMQMessagingService($zmqServer, $zmqPort, $websocketServers));
