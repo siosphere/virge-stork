50ff2de78eb9e5a6b7a458d8cd5f2a86887fc2a6
Michael Kramer <kramer@localhost.localdomain>
2016-04-03 20:03:25 -0700

    Setup command to not start if already started

--- Virge/Stork/Command/RunPublishServerCommand.php
+++ Virge/Stork/Command/RunPublishServerCommand.php
@@ -9,12 +9,16 @@ use Virge\Virge;
 /**
  * listens for incoming ZMQ messages, and then broadcasts those out
  */
-class RunPublishServerCommand
+class RunPublishServerCommand extends \Virge\Cli\Component\Command
 {
     const COMMAND = 'virge:stork:run_publish_server';
     
     public function run()
     {
+        if($this->instanceAlreadyRunning()) {
+            $this->terminate();
+        }
+        
         Cli::output("starting publish server");
         Virge::service(ZMQMessagingService::SERVICE_ID)
             ->startPublishingServer();
